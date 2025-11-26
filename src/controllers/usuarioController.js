e2d1f670023a53e638508471f1e83a2a13607fdb
Philipi <philipi.rodrigues@sptech.school>
2025-11-26 10:44:44 -0300

    Versão final

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -19,20 +19,12 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        // aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                        //     .then((resultadoAquarios) => {
-                        //         if (resultadoAquarios.length > 0) {
                                     res.json({
                                         id: resultadoAutenticar[0].id,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha
-                                        // aquarios: resultadoAquarios
                                     });
-                            //     } else {
-                            //         res.status(204).json({ aquarios: [] });
-                            //     }
-                            // })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -59,7 +51,6 @@ function cadastrar(req, res) {
     var email = req.body.emailServer;
     var data_nascimento = req.body.dt_nascimentoServer;
     var senha = req.body.senhaServer;
-    // var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -77,9 +68,6 @@ function cadastrar(req, res) {
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     }
-    // else if (fkEmpresa == undefined) {
-    //     res.status(400).send("Sua empresa a vincular está undefined!");
-    // } 
     else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
