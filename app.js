049909f5d10b0e773d671e82ed7797cec77e08f8
Philipi <philipi.rodrigues@sptech.school>
2025-11-25 22:26:55 -0300

    Reforma na navbar da dash, adição de validação na tela de cadastro

--- app.js
+++ app.js
@@ -18,9 +18,6 @@ var app = express();
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var dashRouter = require("./src/routes/dash");
-// var avisosRouter = require("./src/routes/avisos");
-// var aquariosRouter = require("./src/routes/aquarios");
-// var empresasRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,9 +28,6 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/dash", dashRouter);
-// app.use("/avisos", avisosRouter);
-// app.use("/aquarios", aquariosRouter);
-// app.use("/empresas", empresasRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
